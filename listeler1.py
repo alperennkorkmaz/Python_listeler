de03da78a89b85916869dc318d5ffdb9da24fa13
alperennkorkmaz <151527086+alperennkorkmaz@users.noreply.github.com>
2023-11-20 14:58:51 +0300

    Update listeler1.py

--- listeler1.py
+++ listeler1.py
@@ -1,5 +1,5 @@
-liste1=["ali","mehmet","recep","ebu ubeyde"]
-liste2=["emre","başkan","dihye","enes"]
+liste1=["ali","mehmet","recep","alperen"]
+liste2=["emre","alparslan","doğukan","enes"]
 while True:
         isim=input("isim giriniz çıkmak için q bas ")
         if isim =="q":
@@ -9,4 +9,4 @@ while True:
         elif isim in liste2:
                 print("aşağı kata")
         elif isim not in liste1 and isim not in liste2:
-                print("siktir git lan burdan")
+                print("İsiminiz listede yoktur...")
